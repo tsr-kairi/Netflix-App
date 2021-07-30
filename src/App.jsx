ee0254e0122842b520b460c24a1f90b17b361799
Ranjit Kairi <ranjitkoiri009@gmail.com>
2021-07-30 17:30:23 +0530

    netflix

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import "./App.scss";
+import React from "react";
+import Home from "./Pages/Home/Home";
+
+const App = () => {
+  return <Home />;
+};
+
+export default App;
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import List from "../../Components/List/List";
+import Featured from "../../Components/Featured/Featured";
+import Navbar from "../../Components/Navbar/Navbar";
+import "./Home.scss";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <Navbar />
+      <Featured />
+      <List />
+      <List />
+      <List />
+      <List />
+    </div>
+  );
+};
+
+export default Home;
